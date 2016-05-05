f1c3e1d0525b663d34413db66616e1ad0e122ac4
draffensperger <d.raffensperger@gmail.com>
2016-05-04 21:47:36 -0400

    Add config require

--- lib/type_tracer.rb
+++ lib/type_tracer.rb
@@ -3,9 +3,10 @@ Dir[File.join(File.dirname(__FILE__), 'type_tracer', '*.rb')].each do |file|
   next if file =~ /version/
   require File.join('type_tracer', File.basename(file, '.rb'))
 end
-require 'type_tracer/arg_send_type_check/ast_checker.rb'
-require 'type_tracer/arg_send_type_check/method_checker.rb'
-require 'type_tracer/arg_send_type_check/runner.rb'
+require 'type_tracer/arg_send_type_check/ast_checker'
+require 'type_tracer/arg_send_type_check/method_checker'
+require 'type_tracer/arg_send_type_check/runner'
+require 'type_tracer/config'
 
 module TypeTracer
   class << self
