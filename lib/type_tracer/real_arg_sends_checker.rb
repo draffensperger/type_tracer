798f0b7294e4097b1db82b6033fca8827f28e414
draffensperger <d.raffensperger@gmail.com>
2016-05-07 15:34:45 -0400

    Remove AstUtil

--- lib/type_tracer/real_arg_sends_checker.rb
+++ lib/type_tracer/real_arg_sends_checker.rb
@@ -1,11 +1,6 @@
 # frozen_string_literal: true
-require 'parser/current'
-require_relative 'ast_util'
-
 module TypeTracer
   class RealArgSendsChecker
-    include AstUtil
-
     def initialize(klass, symbol, args)
       @klass = klass
       @symbol = symbol
@@ -19,7 +14,7 @@ module TypeTracer
     private
 
     def message_if_invalid_arg(arg_value, index)
-      arg_sends = arg_send_list[index]
+      arg_sends = method_analyzer.arg_sends.values[index]
       return unless arg_sends
       arg_sends.each do |arg_send|
         next if arg_value.respond_to?(arg_send)
@@ -30,26 +25,27 @@ module TypeTracer
 
     def invalid_arg_message(arg_value, arg_send, index)
       "Called stubbed method #{@klass}##{@symbol} with unrealistic "\
-        "'#{arg_names[index]}' argument: #{arg_value.inspect}. "\
+        "'#{method_analyzer.arg_names[index]}' argument: #{arg_value.inspect}. "\
         "It should respond to #{arg_send.inspect} but it does not."
     end
 
-    def arg_names
-      @arg_names ||= find_arg_names(method_ast)
-    end
-
-    def arg_send_list
-      @arg_send_list ||= find_arg_sends(method_ast)
+    def method_analyzer
+      @method_analyzer ||= MethodAnalyzer.new(method_def: method_ast)
     end
 
     def method_ast
       @method_ast ||=
         begin
           file, _line = @klass.instance_method(@symbol).source_location
-          ruby_source = File.read(file)
-          ast = Parser::CurrentRuby.parse(ruby_source)
+          ast = TypeTracer.parse_file(file)
           find_method_def(ast, @symbol)
         end
+    end
+
+    def find_method_def(ast, method_symbol)
+      ast.each_descendant.find do |node|
+        node.def_type? && node.children.first == method_symbol
+      end
     end
   end
 end
